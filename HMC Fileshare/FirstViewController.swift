3874401b443fda4b0852bdd8af9de97b3ba2760c
hmcssu <hmcssu@gmail.com>
2015-06-26 16:03:08 -0700

    Better method of interacting with map!

--- HMC Fileshare/FirstViewController.swift
+++ HMC Fileshare/FirstViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    @IBOutlet weak var viewMap: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        var camera = GMSCameraPosition.cameraWithLatitude(34.10566,
-            longitude: -117.70698, zoom: 18)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
         
-        var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(34.10566, -117.70698)
-        marker.title = "You are here"
-        // Snippet adds extra text to the map pointer icon.
-//        marker.snippet = ""
-        marker.map = mapView
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 8.0)
+        viewMap.camera = camera
+        
+//        var camera = GMSCameraPosition.cameraWithLatitude(34.10566,
+//            longitude: -117.70698, zoom: 18)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        self.view = mapView
+//        
+//        var marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(34.10566, -117.70698)
+//        marker.title = "You are here"
+//        // Snippet adds extra text to the map pointer icon.
+////        marker.snippet = ""
+//        marker.map = mapView
         
     }
 }
