d76dea30a811aafc8e385230691dad94af901492
eyassushimelis <eyassushimelis@gmail.com>
2015-06-26 09:44:59 -0700

    First commit!

--- HMC Fileshare/FirstViewController.swift
+++ HMC Fileshare/FirstViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Eyassu Shimelis on 6/25/15.
 //  Copyright (c) 2015 HMC Summer Startup. All rights reserved.
-// Make this a GMSmapView!
+// Make this a GMSmapView
 
 import UIKit
 
