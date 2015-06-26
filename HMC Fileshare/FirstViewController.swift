1fd7149a6e836fa030e523065ea5f9a0c8f9c336
hmcssu <hmcssu@gmail.com>
2015-06-26 16:41:59 -0700

    Terrain options.

--- HMC Fileshare/FirstViewController.swift
+++ HMC Fileshare/FirstViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    @IBAction func changeMapType(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "Map Types", message: "Select map type:", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeNormal
+        }
+        
+        let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeTerrain
+        }
+        
+        let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeHybrid
+        }
+        
+        let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        actionSheet.addAction(normalMapTypeAction)
+        actionSheet.addAction(terrainMapTypeAction)
+        actionSheet.addAction(hybridMapTypeAction)
+        actionSheet.addAction(cancelAction)
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
     @IBOutlet weak var viewMap: GMSMapView!
+    
+    @IBOutlet weak var toolbar: UIToolbar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 8.0)
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(34.106, longitude: -117.2, zoom: 8.0)
         viewMap.camera = camera
         
 //        var camera = GMSCameraPosition.cameraWithLatitude(34.10566,
